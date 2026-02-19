a3dbefafdaac8f8862323fd9526ede0e918c611f
ProRow15 <diamondtsorrells@gmail.com>
2026-02-19 03:26:47 -0500

    Create MobileContent component

---
+++ src/components/MobileContent.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MobileContent = () => {
+    return (
+        <div>
+            <h1>Mobile Content</h1>
+            <p>This is the content displayed on mobile devices.</p>
+        </div>
+    );
+};
+
+export default MobileContent;
