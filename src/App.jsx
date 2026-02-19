722c6da72751a0fa824bec5570753bb54cc61c8f
ProRow15 <diamondtsorrells@gmail.com>
2026-02-19 03:26:01 -0500

    Create src/App.jsx with main React application code

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to the Guest Book Application</h1>
+      </header>
+      <main>
+        <p>This is where you can add your guest comments!</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
